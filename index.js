4a9db592ede05ef219eabe38d1de025902d1ae90
sanleomon <170890793+sanleomon@users.noreply.github.com>
2026-09-08 04:27:45 +0000

    Prepare assets for production build

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const posts = [
         name: "Vincent van Gogh",
         username: "vincey1853",
         location: "Zundert, Netherlands",
-        avatar: "images/avatar-vangogh.jpg",
-        post: "images/post-vangogh.jpg",
+        avatar: "/images/avatar-vangogh.jpg",
+        post: "/images/post-vangogh.jpg",
         comment: "just took a few mushrooms lol",
         likes: 21
     },
@@ -12,8 +12,8 @@ const posts = [
         name: "Gustave Courbet",
         username: "gus1819",
         location: "Ornans, France",
-        avatar: "images/avatar-courbet.jpg",
-        post: "images/post-courbet.jpg",
+        avatar: "/images/avatar-courbet.jpg",
+        post: "/images/post-courbet.jpg",
         comment: "i'm feelin a bit stressed tbh",
         likes: 4
     },
@@ -21,8 +21,8 @@ const posts = [
         name: "Joseph Ducreux",
         username: "jd1735",
         location: "Paris, France",
-        avatar: "images/avatar-ducreux.jpg",
-        post: "images/post-ducreux.jpg",
+        avatar: "/images/avatar-ducreux.jpg",
+        post: "/images/post-ducreux.jpg",
         comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
         likes: 152
     }
@@ -48,15 +48,15 @@ function renderPosts() {
 
                             <div class="actions">
                                 <button class="like-btn" aria-label="Like post">
-                                    <img src="images/icon-heart.png" alt="">
+                                    <img src="/images/icon-heart.png" alt="">
                                 </button>
 
                                 <button class="comment-btn" aria-label="Comment on post">
-                                    <img src="images/icon-comment.png" alt="">
+                                    <img src="/images/icon-comment.png" alt="">
                                 </button>
 
                                 <button class="share-btn" aria-label="Share post">
-                                    <img src="images/icon-dm.png" alt="">
+                                    <img src="/images/icon-dm.png" alt="">
                                 </button>
                             </div>
 
